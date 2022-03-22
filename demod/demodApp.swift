5a9acf82c0b1897d88ac15c532e00479cde07f7b
Isaiah Colson <colson.isaiah@gmail.com>
2022-03-22 18:34:09 -0500

    Initial Commit

---
+++ demod/demodApp.swift
@@ -0,0 +1,17 @@
+//
+//  demodApp.swift
+//  demod
+//
+//  Created by Isaiah Colson on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct demodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
